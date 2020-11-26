817f3b6bb196084492189b631da59c12e978360d
Techoc <543938801@qq,com>
2020-11-26 23:21:47 +0800

    进行项目的前置准备

---
+++ src/com/itqf/entity/User.java
@@ -0,0 +1,40 @@
+package com.itqf.entity;
+
+public class User {
+    private String username;
+    private String code;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", code='" + code + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
